cae6be06101878030ac5f465a4b9c05233f8958f
KLJ <517106763@qq,com>
2017-08-16 15:14:05 +0800

    Dto PO

---
+++ smart-dubbox/system-api/src/main/java/com/ybveg/govx/system/api/AreaService.java
@@ -0,0 +1,12 @@
+package com.ybveg.govx.system.api;
+
+import com.ybveg.govx.system.model.dto.AreaDto;
+
+/**
+ * Created by KLJ on 2017/8/16.
+ */
+public interface AreaService {
+
+    public boolean add(AreaDto area) throws Exception;
+
+}
--- smart-dubbox/system-service/src/main/java/com/ybveg/govx/system/mapper/AreaMapper.java
+++ smart-dubbox/system-service/src/main/java/com/ybveg/govx/system/mapper/AreaMapper.java
@@ -1,10 +1,13 @@
 package com.ybveg.govx.system.mapper;
 
 import com.ybveg.govx.system.model.po.Area;
+import tk.mybatis.mapper.common.Mapper;
 
-public interface AreaMapper {
+public interface AreaMapper extends Mapper<Area> {
 
-  int insert(Area record);
 
-  int insertSelective(Area record);
+//  int insert(Area record);
+
+//  int insertSelective(Area record);
+
 }
