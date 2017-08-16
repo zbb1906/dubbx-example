0bfe97fc4b5e944af406c42240138442b01e6092
zbb <zhangbinbin@ybveg.com>
2017-08-16 09:38:36 +0800

    auth  注释

--- smart-portal/portal-web/src/main/java/com/ybveg/govx/auth/GovxAuthManager.java
+++ smart-portal/portal-web/src/main/java/com/ybveg/govx/auth/GovxAuthManager.java
@@ -1,23 +1,23 @@
-package com.ybveg.govx.auth;
-
-import com.alibaba.dubbo.config.annotation.Reference;
-import com.ybveg.auth.AuthAbstractManager;
-import com.ybveg.auth.model.ModuleModel;
-import java.util.List;
-import org.springframework.stereotype.Component;
-
-/**
- * 实现并获取权限
- *
- * @auther zbb
- * @create 2017/8/15
- */
-@Component
-public class GovxAuthManager extends AuthAbstractManager {
-
-
-  @Override
-  public List<ModuleModel> getAuths(String key) {
-    return null;
-  }
-}
+//package com.ybveg.govx.auth;
+//
+//import com.alibaba.dubbo.config.annotation.Reference;
+//import com.ybveg.auth.AuthAbstractManager;
+//import com.ybveg.auth.model.ModuleModel;
+//import java.util.List;
+//import org.springframework.stereotype.Component;
+//
+///**
+// * 实现并获取权限
+// *
+// * @auther zbb
+// * @create 2017/8/15
+// */
+//@Component
+//public class GovxAuthManager extends AuthAbstractManager {
+//
+//
+//  @Override
+//  public List<ModuleModel> getAuths(String key) {
+//    return null;
+//  }
+//}
