a9b59ac764f8a220d3ff3f4da90ec094890b9b3d
zbb <zhangbinbin@ybveg.com>
2017-08-17 10:23:25 +0800

    去掉模块id

--- smart-dubbox/system-api/src/main/java/com/ybveg/govx/system/model/po/Module.java
+++ smart-dubbox/system-api/src/main/java/com/ybveg/govx/system/model/po/Module.java
@@ -1,11 +1,9 @@
 package com.ybveg.govx.system.model.po;
 
 import com.github.tj123.common.BasePo;
-import com.ybveg.govx.system.model.dto.MenuModuleDto;
 import com.ybveg.govx.system.model.dto.ModuleDto;
 import java.io.Serializable;
 import java.util.Date;
-import javax.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,9 +12,6 @@ import lombok.Setter;
 public class Module extends BasePo<ModuleDto> implements Serializable {
 
   private static final long serialVersionUID = 1L;
-
-  @Id
-  private String id;
 
   private String moduleCode;
 
