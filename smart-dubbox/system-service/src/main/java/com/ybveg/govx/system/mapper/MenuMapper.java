eda88041222fe62965dda01e5d84d4cd46240f62
hb <839072534@qq.com>
2017-08-17 18:19:58 +0800

    module后台代码 menu后台代码

--- smart-dubbox/system-service/src/main/java/com/ybveg/govx/system/mapper/MenuMapper.java
+++ smart-dubbox/system-service/src/main/java/com/ybveg/govx/system/mapper/MenuMapper.java
@@ -1,10 +1,12 @@
 package com.ybveg.govx.system.mapper;
 
 import com.ybveg.govx.system.model.po.Menu;
+import java.util.List;
+import java.util.Map;
+import tk.mybatis.mapper.common.BaseMapper;
+import tk.mybatis.mapper.common.Mapper;
 
-public interface MenuMapper {
+public interface MenuMapper extends Mapper<Menu> {
 
-  int insert(Menu record);
-
-  int insertSelective(Menu record);
+  List<Map<String, Object>> listForPage() throws Exception;
 }
